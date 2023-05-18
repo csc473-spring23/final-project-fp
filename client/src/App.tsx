6bc7292995ee9499b8fba4654f75601577f59327
Maoxi Xu <99984525+Maoxi16@users.noreply.github.com>
2023-05-17 23:16:48 -0400

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { AuthContext } from "../helpers/AuthContext";
-import Nav from 'react-bootstrap/Nav';
-import { BrowserRouter as Router, Route, Switch, Link, useHistory } from "react-router-dom";
-import InBox from './pages/InBox';
-import SendBox from './pages/SendBox';
-import Write from './pages/Write';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import OpenedEmail from './pages/OpenedEmail';
-import Button from 'react-bootstrap/Button';
-
-
-
+import Nav from "react-bootstrap/Nav";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import InBox from "./pages/InBox";
+import SendBox from "./pages/SendBox";
+import Write from "./pages/Write";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import OpenedEmail from "./pages/OpenedEmail";
+import Button from "react-bootstrap/Button";
 
 function App() {
   const [authState, setAuthState] = useState<boolean | null>(false);
@@ -27,68 +30,65 @@ function App() {
     window.location.reload();
   };
 
-  useEffect( () => {
-    axios.get("http://localhost:3001/auth/verify", {headers: {accessToken: localStorage.getItem("accessToken")}})
-    .then((resp) => {
-      if(resp.data.error) setAuthState(false);
-      else setAuthState(true);
-    })
-  }, [])
-
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/auth/verify", {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((resp) => {
+        if (resp.data.error) setAuthState(false);
+        else setAuthState(true);
+      });
+  }, []);
 
   return (
-    <div className = "APP">
+    <div className="APP">
       <AuthContext.Provider value={{ authState, setAuthState }}>
-      <Router>
-      <Nav fill variant="tabs">
-      { authState && (
-        <div>
-      <Nav.Item>
-        <Nav.Link href="/">InBox</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/SendBox">SendBox</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/Write">Write</Nav.Link>
-      </Nav.Item>
-      <Button onClick = {handleClick} >Log out</Button>
-      </div>
-      )
-      }
-      { !authState && (
-        <div>
-      <Nav.Item>
-        <Nav.Link href="/Login">Login</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="/Register">Register</Nav.Link>
-      </Nav.Item>
-      </div>
-      )
-      }
-    </Nav>
+        <Router>
+          <Nav fill variant="tabs">
+            {authState && (
+              <div>
+                <Nav.Item>
+                  <Nav.Link href="/">InBox</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/SendBox">Sent Box</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/Write">Write New Mail</Nav.Link>
+                </Nav.Item>
+                <Button onClick={handleClick}>Log out</Button>
+              </div>
+            )}
+            {!authState && (
+              <div>
+                <Nav.Item>
+                  <Nav.Link href="/Login">Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/Register">Register</Nav.Link>
+                </Nav.Item>
+              </div>
+            )}
+          </Nav>
 
-    { authState && (
-      <Switch>
-        <Route exact path="/" component={InBox} />
-        <Route path="/SendBox" component={SendBox} />
-        <Route path="/Write" component={Write} />
-        <Route path="/emails/:id" component={OpenedEmail} />
-        
-      </Switch>
-    )}
-    { !authState && (
-      <Switch>
-      <Route path = "/Register"exact component={Register}/>
-      <Route path = "/Login" exact component={Login}/>
-      </Switch>
-    )}
-    </Router>
+          {authState && (
+            <Switch>
+              <Route exact path="/" component={InBox} />
+              <Route path="/SendBox" component={SendBox} />
+              <Route path="/Write" component={Write} />
+              <Route path="/emails/:id" component={OpenedEmail} />
+            </Switch>
+          )}
+          {!authState && (
+            <Switch>
+              <Route path="/Register" exact component={Register} />
+              <Route path="/Login" exact component={Login} />
+            </Switch>
+          )}
+        </Router>
       </AuthContext.Provider>
     </div>
-  )
+  );
 }
-export default App
-
-
+export default App;
